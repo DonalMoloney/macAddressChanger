308404be233bfce17e9d575130caee84330ebdec
DonalMoloney <Donalmshs@gmail.com>
2019-01-28 10:31:13 -0600

    Entered parameters as an array, preventing a user from hijacking the program to run other commands such as by inserting a semicolon followed by a command. If a user entered for example an input followed by ;ls the program would crash

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # A simple project that changes the mac address of your computer
 # Created by Donal Moloney on January 7 2019
+
 #!/usr/bin/env python
 
 import subprocess
@@ -9,6 +10,6 @@ new_mac = input("Enter a new MAC address example: 00:11:22:33:44:77\n")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig "+interface+" down", shell = True) #disabling wlan0
-subprocess.call("ifconfig "+interface+" hw ether "+ new_mac, shell = True) #changing mac address of wlan
-subprocess.call("ifconfig "+interface+" up", shell = True) #enabeling wlan0
+subprocess.call(["ifconfig",interface,"down"], shell = True) #disabling wlan0
+subprocess.call(["ifconfig",interface,"hw","ether",new_mac], shell = True) #changing mac address of wlan
+subprocess.call(["ifconfig",interface,"up"], shell = True) #enabeling wlan0
