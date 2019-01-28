48481b947a1e6cdfa2474605d143e9f67d48b570
DonalMoloney <Donalmshs@gmail.com>
2019-01-28 10:47:58 -0600

    Added parser option for user input on new mac address

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import optparse
 
 parser = optparse.OptionParser()
 parser.add_option("-i", "--interface",dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac",dest="new_mac", help="New mac address")
 
 parser.parse_args()
 
